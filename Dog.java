dbc87840ca4f691e3d9fe3e1d3cfc0f052fd9f67
juancarlosramirezmariscal <jr580091@gmail.com>
2019-01-08 13:23:38 -0600

    clase dog

---
+++ Dog.java
@@ -0,0 +1,14 @@
+public class Dog{
+
+ 	/*Atributos
+         raza
+         tamaño
+         color  
+	---------*/
+	/*Metodos
+         ladrar( )
+         caminar( )
+         correr( )
+         comer( )
+	 ---------*/
+}
